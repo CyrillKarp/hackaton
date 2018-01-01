a85d789d675c9c340d08d214e483e56bc5ca4328
Cyrill <torreskirill@yandex.ru>
2018-01-02 01:51:56 +0300

    add files
    add files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function(){
+	$('.collapse').on('show.bs.collapse', function(){
+		$(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle')
+	});
+	$('.collapse').on('hide.bs.collapse', function(){
+		$(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle')
+	});
+	$('#myForm').validator({
+		feedback: {
+		  success: 'fa fa-check-circle',
+		  error: 'fa fa-times-circle'
+		}
+	});
+	new WOW().init();
+});
