baad033cd044643a6609a48adb60561f9ccd15a1
Cyrill <torreskirill@yandex.ru>
2018-01-03 18:19:53 +0300

    Added a smooth scroll

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 It is a web site for event or meeting.
 
-What have I done: <br>
-    1. Redesign PSD template.<br>
+What did I do: <br>
+    1. Redesigned PSD template.<br>
     2. Adaptive layout with Bootstrap.<br>
-    3. Add animation (animate css + WOW.js).<br>
+    3. Added animation (animate css + WOW.js).<br>
     4. Form validator.<br>
-    5. Add Google maps. <br>
-    6. Add social-likes plugin for social buttons. <br>
+    5. Added Google maps. <br>
+    6. Added social-likes plugin for social buttons. <br>
+    7. Added a smooth scroll.
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,30 @@ $(function(){
 	$('.collapse').on('hide.bs.collapse', function(){
 		$(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle')
 	});
+    
 	$('#myForm').validator({
 		feedback: {
 		  success: 'fa fa-check-circle',
 		  error: 'fa fa-times-circle'
 		}
 	});
+    
 	new WOW().init();
+    
+     $(".js-register-btn").click(function() {
+                
+        var scrollToAbout = $("#js-register-form").offset().top;
+        
+        $("html, body").animate({
+           scrollTop:  scrollToAbout
+        }, 1000);
+    });
+        $("#js-discover-btn").click(function() {
+                
+        var scrollToAbout = $(".speakers").offset().top;
+        
+        $("html, body").animate({
+           scrollTop:  scrollToAbout
+        }, 800);
+    });
 });
